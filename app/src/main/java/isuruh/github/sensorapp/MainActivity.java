56b315b0d4b5a94f478ee79ad2114d54e76d6af6
isuruh15 <isuruhemantha.15@cse.mrt.ac.lk>
2019-09-14 18:04:08 +0530

    working,threshould vibration added

--- app/src/main/java/isuruh/github/sensorapp/MainActivity.java
+++ app/src/main/java/isuruh/github/sensorapp/MainActivity.java
@@ -1,18 +1,45 @@
 package isuruh.github.sensorapp;
 
-import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Vibrator;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import java.lang.Math;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float deltaX = 0;
+    private float deltaY = 0;
+    private float deltaZ = 0;
+
+    private float lastX = 0;
+    private float lastY = 0;
+    private float lastZ = 0;
+
+    private TextView xLabel;
+    private TextView yLabel;
+    private TextView zLabel;
+
+    public Vibrator v;
+    private float vibrateThreshold = 0;
+    private float movementThreshold = 0.5f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +48,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        xLabel = findViewById(R.id.lbl_x);
+        yLabel = findViewById(R.id.lbl_y);
+        zLabel = findViewById(R.id.lbl_z);
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            // success! we have an accelerometer
+
+            accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            //sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            vibrateThreshold = accelerometer.getMaximumRange() / 2;
+            System.out.println("initialized");
+
+        } else {
+            // fail we don't have an accelerometer!
+            System.out.println("init failed");
+        }
+
+        //initialize vibration
+        v = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+
+
+
     }
 
     @Override
@@ -52,4 +96,56 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        deltaX = Math.abs(lastX - sensorEvent.values[0]);
+        deltaY = Math.abs(lastY - sensorEvent.values[1]);
+        deltaZ = Math.abs(lastZ - sensorEvent.values[2]);
+
+        // if the change is below 1, it is just plain noise
+        if (deltaX < movementThreshold)
+            deltaX = 0;
+        if (deltaY < movementThreshold)
+            deltaY = 0;
+        if (deltaZ < movementThreshold)
+            deltaZ = 0;
+
+
+        // set the last know values of x,y,z
+        lastX = sensorEvent.values[0];
+        lastY = sensorEvent.values[1];
+        lastZ = sensorEvent.values[2];
+
+
+        xLabel.setText(Float.toString(deltaX));
+        yLabel.setText(Float.toString(deltaY));
+        zLabel.setText(Float.toString(deltaZ));
+
+        vibrate();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        System.out.println("accuracy changed");
+    }
+
+    public void onClickStart(View view){
+        System.out.println("starting");
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onClickStop(View view){
+        System.out.println("stopping");
+        sensorManager.unregisterListener(this);
+    }
+
+    public void vibrate() {
+        if ((deltaX > vibrateThreshold) || (deltaY > vibrateThreshold) || (deltaZ > vibrateThreshold)) {
+            v.vibrate(50);
+        }
+    }
+
 }
